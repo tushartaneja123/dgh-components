98e3f775dfbe58a03e65dec4c507edc1a9a94aa5
tushartaneja <tushartaneja61@gmail.com>
2025-03-31 17:36:48 +0530

    Searchable dropdown

--- src/app/pages/form-field/form-field.component.ts
+++ src/app/pages/form-field/form-field.component.ts
@@ -1,33 +1,76 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { MaterialModule } from '../../shared/material.module';
-import { FormControl } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-form-field',
   standalone: true,
-  imports: [MaterialModule],
+  imports: [MaterialModule, NgIf],
   templateUrl: './form-field.component.html',
-  styleUrl: './form-field.component.scss'
+  styleUrl: './form-field.component.scss',
 })
-export class FormFieldComponent {
+export class FormFieldComponent implements OnInit{
 
   @ViewChild('input') input!: ElementRef<HTMLInputElement>;
-    
-    myControl = new FormControl('');
-    filteredOptions: string[];
-    options: string[] = ['One', 'Two', 'Three', 'Four', 'Five'];
-  
-    constructor() {
-      this.filteredOptions = this.options.slice();
-    }
-  
-  
-    filter(): void {
-      const filterValue = this.input.nativeElement.value.toLowerCase();
-      this.filteredOptions = this.options.filter(o => o.toLowerCase().includes(filterValue));
-    }
+  @ViewChild('searchTextBox') searchTextBox!: ElementRef;
 
+  options: string[] = ['One', 'Two', 'Three', 'Four', 'Five'];
+  filteredOptions: string[];
 
-    
+  multipleSelectForm!: FormGroup;
+  myControl = new FormControl('');
+
+  selectedToppingList: any[] = [
+    { name: 'Abhishek', id: 1 },
+    { name: 'Bharat', id: 2 },
+    { name: 'Chetan', id: 3 },
+    { name: 'Deepak', id: 4 },
+    { name: 'Enable', id: 5 },
+    { name: 'Google', id: 6 },
+    { name: 'Hariom', id: 7 },
+  ];
+
+  toppingList: any[] = [...this.selectedToppingList];
+
+  constructor(private _formBuilder: FormBuilder) {
+    this.filteredOptions = this.options.slice();
+  }
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.multipleSelectForm = this._formBuilder.group({
+      dropdownControl: [''],
+    });
+  }
+
+  filter(): void {
+    const filterValue = this.input.nativeElement.value.toLowerCase();
+    this.filteredOptions = this.options.filter(o => o.toLowerCase().includes(filterValue));
+  }
+
+  filterApply(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value.toLowerCase();
+    this.selectedToppingList = this.toppingList.filter((tooping: any) => {
+      return tooping.name.toLowerCase().includes(filterValue);
+    });
+  }
+
+  clearSearchBox() {
+    this.searchTextBox.nativeElement.value = '';
+    this.selectedToppingList = [...this.toppingList];
+  }
+
+  openedChange(event: any) {
+    this.searchTextBox.nativeElement.value = '';
+    this.selectedToppingList = [...this.toppingList];
+    if (event === true) {
+      this.searchTextBox.nativeElement.focus();
+    }
+  }
+
 
 }
